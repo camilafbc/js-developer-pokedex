b2a148c1dccb971f980636bf378989dcdfbdad12
camilafbc <camilafernanda914@gmail.com>
2023-06-15 23:51:48 -0300

    iniciando alterações

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,9 @@ function convertPokemonsToList(pokemon){
    <a href="details.html?id=${pokemon.number}" id="goToDetail">
             <li id="goToDetail" class="pokemon ${pokemon.type}" >
                 <div class="pokemon-card" id="${pokemon.number}">  
-                    <span class="pokemon-card-number ${pokemon.type}">#0${pokemon.number}</span>
+                    <span class="pokemon-card-number ${pokemon.type}">
+                        #0${pokemon.number}
+                    </span>
                     <img src=${pokemon.image} alt="${pokemon.name}" class="pokemon-card-img">
                     <span class="pokemon-card-name ${pokemon.type}">${pokemon.name}</span>  
                 </div>
