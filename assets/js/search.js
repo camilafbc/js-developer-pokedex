d33bde64fb59ff29aa97554094edbb82a0f2293c
camilafbc <camilafernanda914@gmail.com>
2023-06-21 00:09:38 -0300

    barra de pesquisa

--- assets/js/search.js
+++ assets/js/search.js
@@ -2,16 +2,13 @@ import { pokeApi } from "./poke-api.js";
 import { convertPokemonsToList } from "./convertPokemonsToList.js";
 import { cardListeners } from "./modal.js";
 
-// const searchBtn = document.getElementById("search-btn");
-// const input = document.querySelector("input[name='search']");
 const list = document.getElementById('listPokemons');
 
-
 function realizarBusca(entrada) {
-    // Verificar se a entrada é um número
+    // Verifica se a entrada é um número
     if (/^\d+$/.test(entrada)) {
         let format = parseInt(entrada)
-        console.log(format)
+ 
         pokeApi.getPokemons(format - 1, 1).then((pokemonsList = []) => {
 
             const newList = pokemonsList.map((pokemon) => convertPokemonsToList(pokemon))
@@ -23,10 +20,14 @@ function realizarBusca(entrada) {
             cardListeners()
         })
     } else {
-        console.log("Não é número")
-        pokeApi.getPokemonForName(entrada).then((Pokemon) => console.log(Pokemon))
-        console.log(entrada)
-        console.log(pokeApi.getPokemonForName(entrada).then(result))
+
+        pokeApi.getPokemonForName(entrada).then((Pokemon) => {
+            const newList = convertPokemonsToList(Pokemon)
+    
+            list.innerHTML = newList
+    
+            cardListeners()
+        })
     }
 }
 
